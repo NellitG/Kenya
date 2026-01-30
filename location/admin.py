985909128d8832548ab874525d33327d429b6983
NellitG <kamaunelson157@gmail.com>
2026-01-30 11:59:37 +0300

    create admin interface

--- location/admin.py
+++ location/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import County, SubCounty, Ward
 
-# Register your models here.
+@admin.register(County)
+class CountyAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(SubCounty)
+class SubCountyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'county']
+    list_filter = ['county']
+    search_fields = ['name']
+
+
+@admin.register(Ward)
+class WardAdmin(admin.ModelAdmin):
+    list_display = ['name', 'subcounty']
+    list_filter = ['subcounty__county']
+    search_fields = ['name']
